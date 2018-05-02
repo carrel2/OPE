653f5b24a5c92faa949af0551f38426766394a4e
Carrel <carrel2@illinois.edu>
2018-05-02 13:03:23 -0500

    add bulma form theme, update form types

--- src/AppBundle/Form/AttendeeType.php
+++ src/AppBundle/Form/AttendeeType.php
@@ -10,12 +10,26 @@ class AttendeeType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('firstname')
-          ->add('middleinitial')
-          ->add('lastname')
-          ->add('email')
-          ->add('phonenumber')
-          ->add('opeEvents');
+        $builder->add('firstname', null, array(
+          'label' => 'First name',
+        ))
+        ->add('middleinitial', null, array(
+          'label' => 'Middle initial',
+        ))
+        ->add('lastname', null, array(
+          'label' => 'Last name',
+        ))
+        ->add('email')
+        ->add('phonenumber', null, array(
+          'label' => 'Phone number',
+        ))
+        ->add('opeEvents', 'Symfony\Bridge\Doctrine\Form\Type\EntityType', array(
+          'class' => 'AppBundle:OPEEvent',
+          'label' => 'Events attended',
+          'choice_label' => 'title',
+          'multiple' => true,
+          'required' => false,
+        ));
     }
 
     public function configureOptions(OptionsResolver $resolver)
--- src/AppBundle/Form/OPEEventType.php
+++ src/AppBundle/Form/OPEEventType.php
@@ -11,8 +11,17 @@ class OPEEventType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->add('title')
-          ->add('dates')
-          ->add('attendees');
+          ->add('dates', 'Symfony\Component\Form\Extension\Core\Type\CollectionType', array(
+            'entry_type' => 'Symfony\Component\Form\Extension\Core\Type\DateType',
+            'allow_add' => true,
+            'allow_delete' => true,
+          ))
+          ->add('attendees', 'Symfony\Bridge\Doctrine\Form\Type\EntityType', array(
+            'class' => 'AppBundle:Attendee',
+            'choice_label' => 'fullName',
+            'multiple' => true,
+            'required' => false,
+          ));
     }
 
     public function configureOptions(OptionsResolver $resolver)
